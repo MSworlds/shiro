7f95670cf84a9f93f1f01f54f0b3d1f88a93a9fa
shuaimeng <980472035@qq.com>
2019-04-06 14:38:27 +0800

    u

--- Hellowdemo/src/main/Test/TestGit2.java
+++ Hellowdemo/src/main/Test/TestGit2.java
@@ -9,14 +9,11 @@ public class TestGit2 {
         //111
         //ide1111
         //指提交一个
-        System.out.println("只提交一个 file2");
-                //指提交一个3
+
+
+
 
         //
-        //
-        //1
-
-        //222
 
 
     }
